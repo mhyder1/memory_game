d70706bf6376d48a202dadc2442aab565865cca7
mhyder1 <muhammadabdusamad@yahoo.com>
2022-10-04 21:56:52 -0400

    header, footer an board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
